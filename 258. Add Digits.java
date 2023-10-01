e5cf6a6fc7fdd4feedbf5fba3cab2aaf979f74ba
TEMPEST <88716981+T3MPES7@users.noreply.github.com>
2023-10-01 18:35:45 +0100

    Create 258. Add Digits.java

---
+++ 258. Add Digits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num==0)return 0; //when the integer is 0 returns 0
+        
+        if(num%9==0)return 9; //when the integer is divisible by 9, 9 is returned 
+        
+        else return num%9; //returns the digital root of any other integer
+    }
+}
